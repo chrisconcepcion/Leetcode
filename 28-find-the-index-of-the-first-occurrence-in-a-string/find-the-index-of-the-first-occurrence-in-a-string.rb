5e41b42ec9853dcd9131ebc82af7627ad24c44e3
Christopher Concepcion <christopher.e.concepcion@gmail.com>
2024-12-28 06:49:44 -0500

    Time: 3 ms (19.86%) | Memory: 211.6 MB (36.64%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.rb
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.rb
@@ -2,6 +2,59 @@
 # @param {String} needle
 # @return {Integer}
 def str_str(haystack, needle)
+    i = 0
+    j_start = 0
+    j = 0
+    next_attempts = []
+    while i < haystack.size
+        
+        if needle.size == 1 and haystack[i] == needle[j]
+            return i
+        end
+        
+
+        if j > 0  
+            
+            if haystack[i] == needle[j]
+                j += 1
+                if needle[j] == nil
+                    return j_start
+                end
+            else
+                j = 0
+                if next_attempts.size > 0
+                    i = next_attempts.shift
+                    j_start = i
+                end 
+            end
+        end
+
+        if haystack[i] == needle[0]
+            if j > 0
+                next_attempts << i
+            end
+
+            if j == 0
+                j += 1
+                j_start = i
+            end
+        end
+
+        
+
+        
+        i += 1
+    end
+
+     if needle[j] == nil
+                    return j_start
+                end
+
+    -1
+end
+
+
+def str_str_2(haystack, needle)
   return 0 if needle.length == 0
   
   i = 0
